567bc1cd401909f8a767a660fafba74b8892635c
Hamza Sood <hamzasood@Hamzas-MacBook-Pro-2.local>
2013-07-19 15:03:00 +0100

    Initial Commit

---
+++ AnimatedWallpaperTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AnimatedWallpaperTest
+//
+//  Created by Hamza Sood on 19/07/2013.
+//  Copyright (c) 2013 Hamza Sood. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ AnimatedWallpaperTestTests/AnimatedWallpaperTestTests.m
@@ -0,0 +1,36 @@
+//
+//  AnimatedWallpaperTestTests.m
+//  AnimatedWallpaperTestTests
+//
+//  Created by Hamza Sood on 19/07/2013.
+//  Copyright (c) 2013 Hamza Sood. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface AnimatedWallpaperTestTests : XCTestCase
+
+@end
+
+@implementation AnimatedWallpaperTestTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
